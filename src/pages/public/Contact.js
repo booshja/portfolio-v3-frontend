dfe232250c57680d18b7e3555e0cefef68c4335d
Jacob Andes <jacob.andes@gmail.com>
2022-03-02 20:15:04 -0800

    refactor: contact page aside from recaptcha and api

--- src/pages/public/Contact.js
+++ src/pages/public/Contact.js
@@ -1,5 +1,5 @@
 // dependencies
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { useForm } from "react-hook-form";
 // components
@@ -103,62 +103,79 @@ const Contact = () => {
     formState: { errors },
   } = useForm();
 
+  const [loading, setLoading] = useState(false);
+  const [approval, setApproval] = useState(false);
+  const [error, setError] = useState(false);
+
   const handleDataSubmit = (data) => {
-    // check against recaptcha here, then send data.
-    console.log("Sending data...", data);
-    console.log("..........");
-    console.log("Data Sent.");
+    setLoading(true);
+    try {
+      // check against recaptcha
+      // send data to API
+      setApproval(true);
+    } catch (err) {
+      console.log(err);
+      setError(true);
+    }
+    setLoading(false);
   };
 
   return (
     <ContactContainer>
       <PageTitle>contact()</PageTitle>
-      <MainContent>
-        <LeftSide>
-          <ContactText>
-            Have a question, thoughts, or want to work together?
-          </ContactText>
-          <ContactForm
-            onSubmit={() => handleSubmit((data) => handleDataSubmit(data))}
-          >
-            <ContactLabel htmlFor="name">Name:</ContactLabel>
-            <ContactInput
-              type="text"
-              placeholder="Namey McNameson"
-              id="name"
-              {...register("name", { required: "Name is required." })}
-            />
-            {errors.name && <FormError>{errors.name.message}</FormError>}
-            <ContactLabel htmlFor="email">Email:</ContactLabel>
-            <ContactInput
-              type="text"
-              placeholder="example@email.com"
-              id="email"
-              {...register("email", { required: "Email is required." })}
-            />
-            {errors.email && <FormError>{errors.email.message}</FormError>}
-            <ContactLabel htmlFor="name">Name:</ContactLabel>
-            <ContactTextArea
-              placeholder="I had an idea for you..."
-              id="message"
-              maxLength="200"
-              rows="4"
-              {...register("message", {
-                required: "Message is required.",
-                maxLength: {
-                  value: 200,
-                  message: "Maximum message length 200 characters.",
-                },
-              })}
-            />
-            {errors.message && <FormError>{errors.message.message}</FormError>}
-            <FormSubmitBtn type="submit">Submit</FormSubmitBtn>
-          </ContactForm>
-        </LeftSide>
-        <RightSide>
-          <MailImg src={MailBoxes} alt="Wall of blue P.O. Boxes." />
-        </RightSide>
-      </MainContent>
+      {/* {loading && <LoadingSpinner />} */}
+      {/* {approval && <Approval />} */}
+      {/* {error && <Error />} */}
+      {!loading && !approval && !error && (
+        <MainContent>
+          <LeftSide>
+            <ContactText>
+              Have a question, thoughts, or want to work together?
+            </ContactText>
+            <ContactForm
+              onSubmit={handleSubmit((data) => handleDataSubmit(data))}
+            >
+              <ContactLabel htmlFor="name">Name:</ContactLabel>
+              <ContactInput
+                type="text"
+                placeholder="Namey McNameson"
+                id="name"
+                {...register("name", { required: "Name is required." })}
+              />
+              {errors.name && <FormError>{errors.name.message}</FormError>}
+              <ContactLabel htmlFor="email">Email:</ContactLabel>
+              <ContactInput
+                type="text"
+                placeholder="example@email.com"
+                id="email"
+                {...register("email", { required: "Email is required." })}
+              />
+              {errors.email && <FormError>{errors.email.message}</FormError>}
+              <ContactLabel htmlFor="name">Name:</ContactLabel>
+              <ContactTextArea
+                placeholder="I had an idea for you..."
+                id="message"
+                maxLength="200"
+                rows="4"
+                {...register("message", {
+                  required: "Message is required.",
+                  maxLength: {
+                    value: 200,
+                    message: "Maximum message length 200 characters.",
+                  },
+                })}
+              />
+              {errors.message && (
+                <FormError>{errors.message.message}</FormError>
+              )}
+              <FormSubmitBtn type="submit">Submit</FormSubmitBtn>
+            </ContactForm>
+          </LeftSide>
+          <RightSide>
+            <MailImg src={MailBoxes} alt="Wall of blue P.O. Boxes." />
+          </RightSide>
+        </MainContent>
+      )}
     </ContactContainer>
   );
 };
